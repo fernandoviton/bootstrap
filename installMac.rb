fa3fdcb4d7c365746f35470b8a711326ab7e3f53
Fernndo Viton <fcode@viton.org>
2016-11-19 23:21:03 -0500

    Add elm and electron

--- installMac.rb
+++ installMac.rb
@@ -4,9 +4,15 @@ def run(command)
 end
 
 run "brew update"
-run "brew install bash-completion hub git rbenv ruby-build"
+run "brew install bash-completion hub git node rbenv ruby-build"
 
 # python stuff
 run "brew install python pyenv"
 run "pip install virtualenv"
 # run "curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python"
+
+# elm
+run "npm install -g elm"
+
+# electron
+run "npm install -g electron"
