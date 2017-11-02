b46629dafcbbe6304e2817d71b17dc0e0950a989
fernandoviton <fcode@viton.org>
2017-11-02 16:21:45 -0700

    Update setup_dev_machine.ps1

--- Powershell/setup_dev_machine.ps1
+++ Powershell/setup_dev_machine.ps1
@@ -16,8 +16,9 @@ cinst slack -y
 cinst google-chrome-x64 -y
 cinst notepadplusplus -y
 cinst notepad2 -y
- cinst git -y
- cinst github -y
+cinst git -y
+cinst github -y
+cinst poshgit -y
 
 # Setup Powershell symlinks
 cmd /c mklink /D  "$env:USERPROFILE\Documents\WindowsPowerShell\" "$PSScriptRoot"
